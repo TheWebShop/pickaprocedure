16571a7ab06120b5585c8ad6bdff510aee47d8bc
Kevin Attfield <k.attfield@gmail.com>
2013-08-21 13:38:02 -0700

    added region1 branch

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,7 +21,7 @@ module.exports = function (grunt) {
     var yeomanConfig = {
         app: 'app',
         dist: 'dist',
-        sp: '/Volumes/theloop.gov.bc.ca/rsd/region5/SitePages/procedures'
+        sp: '/Volumes/theloop.gov.bc.ca/rsd/region1/SitePages/procedures'
     };
 
     grunt.initConfig({
